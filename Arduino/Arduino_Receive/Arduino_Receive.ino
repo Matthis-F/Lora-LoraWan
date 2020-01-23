4027093fedb96492ed356acc2b3ea9687f03e792
Matthis-F <Foulonneau.Matthis@gmail.com>
2020-01-23 11:06:51 +0100

    Moved Arduino programs

---
+++ Arduino/Arduino_Receive/Arduino_Receive.ino
@@ -0,0 +1,57 @@
+
+#include <SX1272.h>
+#include <SPI.h>
+#include <Wire.h>
+int e;
+
+
+
+void setup() {
+  //Configure LORA Module
+  sx1272.ON(); //Power on the module
+  sx1272._needPABOOST = true; //The module we have need PA_BOOST in order to work correctly
+#define SX1272_debug_mode=2 //Put on Debug mode (1 partial debug) (2 Full debug)
+  sx1272.setMode(1);//Lora mode correspond to a predefined combinaison of Spreading Factor and Bandwidth
+  sx1272.setPowerDBM((uint8_t)14);//Set MAX Dbm value to 14Dbm
+  sx1272.setChannel((uint32_t)CH_10_868);//Using Channel 10 of 868Mhz band for Europe regulation
+  sx1272.setNodeAddress(2);//Set node address to 10
+  sx1272._enableCarrierSense = true; //enable carrier sense in case of debugging needed
+
+
+  Serial.println("Device successfully configured");
+
+}
+void loop() {
+
+  
+  String data;
+  
+  e = sx1272.receivePacketTimeout(10000);//Open a 10s time window
+  
+ if( e == 0 ){
+      String data;
+      int packet_nb = sx1272.packet_received.packnum;
+      int packet_length = sx1272.packet_received.length;//return legth of received packet
+      Serial.println("Incoming Packet");
+  
+      for (unsigned int i = 0; i < (packet_length)-1 ; i++)//Used to read payload data (returned as char array) and put it into a String
+      {
+        data = data + ((char)sx1272.packet_received.data[i]);
+      }
+      Serial.print("Payload of packet :");
+      Serial.println(data);//prints payload
+      Serial.print("Packet nubmer :");
+      Serial.println(packet_nb);
+
+  }
+  else if( e == 3 ){
+      Serial.println("No packet received during time window");
+
+  }
+ 
+   else{
+      char msg_error;
+      sprintf(msg_error,"An error occured, error code:%d",e);
+      Serial.println(msg_error);
+   }
+}
---
+++ Arduino/Arduino_Send_OLED/Arduino_Send_OLED.ino
@@ -0,0 +1,75 @@
+
+#include <SX1272.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+//Map buttons to pins (Oled Screen)
+#define BUTTON_A  9
+#define BUTTON_B  6
+#define BUTTON_C  5
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+String data;
+
+void setup() {
+  //Configure LORA Module
+  sx1272.ON(); //Power on the module
+  sx1272._needPABOOST=true; //The module we have need PA_BOOST in order to work correctly
+  #define SX1272_debug_mode=2 //Put on Debug mode (1 partial debug) (2 Full debug)
+  sx1272.setMode(1);//Lora mode correspond to a predefined combinaison of Spreading Factor and Bandwidth
+  sx1272.setPowerDBM((uint8_t)14);//Set MAX Dbm value to 14Dbm
+  sx1272.setChannel((uint32_t)CH_10_868);//Using Channel 10 of 868Mhz band for Europe regulation
+  sx1272.setNodeAddress(10);//Set node address to 10
+  sx1272._enableCarrierSense=true;
+ 
+  //Configure Oled Screen
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("<-- Send A");
+  display.setCursor(0,20);
+  display.println("<-- Send B");
+  display.display();
+  
+  
+  Serial.println("Device successfully configured");
+  
+}
+void loop() {
+    display.setCursor(0,0);
+    sx1272.setPacketType(PKT_TYPE_DATA);//Setting the type of the packet to send here we are sending 
+    //DATA so PTK_TYPE_DATE seems appropriate PKT_TYPE8ACK is used for an ACK
+    uint8_t r_size;//declare variable r_size 
+    int e; //declare variable e
+    uint8_t message[100];//declare ariable message
+
+    if(!digitalRead(BUTTON_A)){
+    //reset display
+    display.clearDisplay();
+    display.setCursor(0,20);
+    
+    r_size = sprintf((char*)message, "Boutton A");
+    Serial.println(e);
+    e = sx1272.sendPacketTimeout(2, message, r_size);//Send the Packet
+    display.println("Packet Sent !");
+    }
+
+    if(!digitalRead(BUTTON_B)){
+    //reset display
+    display.clearDisplay();
+    display.setCursor(0,20);
+    
+    r_size = sprintf((char*)message, "Boutton B");
+    Serial.println(e);
+    e = sx1272.sendPacketTimeout(2, message, r_size);//Send the Packet
+    display.println("Packet Sent !");
+    }
+    
+    display.display();
+    
+}
