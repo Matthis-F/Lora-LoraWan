1ee75ec6713f7743b84573802273727a154a16d0
mplayer143 <58776232+mplayer143@users.noreply.github.com>
2020-02-18 08:49:58 +0100

    Update Node_Router_Routing_Static.ino

--- Arduino/Node_Router_Routing_Static/Node_Router_Routing_Static.ino
+++ Arduino/Node_Router_Routing_Static/Node_Router_Routing_Static.ino
@@ -5,30 +5,27 @@
 #include <Wire.h>
 
 //----------------------------------------------------------------------------//
-  //Our RFM95 definition :
+
+//Our RFM95 definition :
 #define RFM95_CS 8 //Chip select radio pin
 #define RFM95_RST 4 //Reset radio pin
 #define RFM95_INT 7 //GPIO/IRQ radio pin
-//----------------------------------------------------------------------------//
 
+//----------------------------------------------------------------------------//
 
 #define RF95_FREQ 868.1 //Match EU regulations
 
-
 //----------------------------------------------------------------------------//
+
 //Node address definition :
 #define CLIENT_ADDRESS 100
 #define ROUTER_ADDRESS 10
 #define GATEWAY_ADDRESS 1
+
 //----------------------------------------------------------------------------//
 
-#define RH_HAVE_SERIAL
-
 RH_RF95 rf95(RFM95_CS, RFM95_INT); //Creating an instance of the RF95 Manager
-RHRouter manager_routing(rf95, ROUTER_ADDRESS);//Creating an instance of the
-//Routing manager
-
-
+RHRouter manager_routing(rf95, ROUTER_ADDRESS); //Creating an instance of the routing manager
 
 void setup() 
 {
@@ -41,15 +38,14 @@ void setup()
 
   delay(100); //Waiting for 100 ms
 
-
   //Manual reset : 
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
-
 //----------------------------------------------------------------------------//
+  
   //Initialisation :
   while (!manager_routing.init()){
     while (1);
@@ -63,47 +59,47 @@ void setup()
   //Set the power :
   rf95.setTxPower(20,false);//Our module is using PA_BOOST so we can go up to 20dBm
   //Unless your radio module can take advantage of PA_BOOST set this value to 13dBm
+  
 //----------------------------------------------------------------------------//
 
-
-
-//----------------------------------------------------------------------------//
- //Static Routing table :
-  manager_routing.addRouteTo(CLIENT_ADDRESS, CLIENT_ADDRESS);
-  manager_routing.addRouteTo(GATEWAY_ADDRESS, GATEWAY_ADDRESS);
+  //Static Routing table :
+  //To join the destination, we have to pass through the next hop
+  manager_routing.addRouteTo(CLIENT_ADDRESS, CLIENT_ADDRESS);  //To join CLIENT_ADDRESS -> next hop : CLIENT_ADDRESS
+  manager_routing.addRouteTo(GATEWAY_ADDRESS, GATEWAY_ADDRESS); //To join GATEWAY_ADDRESS -> next hop : GATEWAY_ADDRESS
+  
 //----------------------------------------------------------------------------//
 
 }
 
 //----------------------------------------------------------------------------//
-  //Sending or Relaying a Packet
+
+//Sending or Relaying a Packet
 void loop(){
 
   //Declaration of variables :
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len=sizeof(buf);
-  uint8_t source=manager_routing.headerFrom();
-  uint8_t dest=manager_routing.headerTo();
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]; //Size of the buffer
+  uint8_t len=sizeof(buf); //Lenght of the buffer
+  uint8_t source=manager_routing.headerFrom(); //Returns the FROM header of the last received message
+  uint8_t dest=manager_routing.headerTo(); //Returns the TO header of the last received message
   
   //Relaying the message
+  //Here we use the function "recvfromACK" of RHDatagram to see if the packet is received or not
+  //Of course the packet could be for the ROUTER_ADDRESS (this node) or not
   bool res = manager_routing.recvfromAck(buf, &len, &source, &dest);
   
+  //Check the boolean result of res
   if (res == 0){
     
-    Serial.println("Packet received");//If the sendtoWait succeed
+    Serial.println("Packet received"); //If the sendtoWait succeed
     
   }
   else {
-    Serial.println("Reception failed");//If the sendtoWait failed 
+    
+    Serial.println("Reception failed"); //If the sendtoWait failed 
+  
   }
   //Putting arduino to sleep for 2 sec (2000ms)
   delay(2000);
 }
 
 //----------------------------------------------------------------------------//
-
-
-  
-  
-
-  
