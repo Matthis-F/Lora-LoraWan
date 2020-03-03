c934a81382a3d67bf528b41dc7c6a9c1970c83ca
mplayer143 <58776232+mplayer143@users.noreply.github.com>
2020-03-03 17:12:51 +0100

    Update Client_2.ino

--- Arduino/Arduino_Receive/Client_2.ino
+++ Arduino/Arduino_Receive/Client_2.ino
@@ -4,20 +4,28 @@
 #include <RH_RF95.h>
 #include <Wire.h>
 
+//----------------------------------------------------------------------------//
+
 //Our RFM95 definition :
 #define RFM95_CS 8 //Chip select radio pin
 #define RFM95_RST 4 //Reset radio pin
 #define RFM95_INT 7 //GPIOO/IRQ radio pin
 #define RF95_FREQ 868.1 //European frequency's used
 
+//----------------------------------------------------------------------------//
+
 //Node address definition :
 #define CLIENT1_ADDRESS 100 //The other node
 #define CLIENT2_ADDRESS 10 //This node
 #define SERVER_ADDRESS 1 //The Gateway 
 
+//----------------------------------------------------------------------------//
+
 //Fonctions needed : 
 RH_RF95 rf95(RFM95_CS, RFM95_INT); //Create an instance of a driver
 RHReliableDatagram manager(rf95, SERVER_ADDRESS);
+
+//----------------------------------------------------------------------------//
 
 //Arduino's setup : 
 void setup() 
@@ -30,50 +38,57 @@ void setup()
 
   delay(100); //Waiting for 100 ms
 
-  Serial.println("Feather 32u4 Lora Tx Test !");
-
+//----------------------------------------------------------------------------//
+  
   //Manual reset : 
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
+//----------------------------------------------------------------------------//  
+  
   //Initialisation :
   while (!manager.init()){
-    Serial.println("LoRa Radio init failed ... ");
-    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
 
-  Serial.println("LoRa Radio init OK !");
-
+//----------------------------------------------------------------------------//  
+  
   //Set our frequency : 
   if (!rf95.setFrequency(RF95_FREQ)){
-    Serial.println("Setting Frequency failed ...");
     while (1);
   }
 
-  Serial.print("Set Frequency to : ");
-  Serial.print(RF95_FREQ);
-  Serial.println(" MHz");
-
+//----------------------------------------------------------------------------//
+  
   //Set the power :
-  rf95.setTxPower(20,false);
+  rf95.setTxPower(20,false);//Our module is using PA_BOOST so we can go up to 20dBm
+  //Unless your radio module can take advantage of PA_BOOST set this value to 13dBm and set "true"
 }
 
+//----------------------------------------------------------------------------//
+
+//Code for reception of packets :
 void loop(){
 
   //Declaration of variables :
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len=sizeof(buf);
-  uint8_t from=manager.headerFrom();
-  uint8_t to=manager.headerTo();
-  uint8_t id=manager.headerId();
-  uint8_t flags=manager.headerFlags();
-  int8_t rssi=rf95.lastRssi();
-  int count = 0;
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN]; //Buffer
+  uint8_t len=sizeof(buf); //Size of buffer
+  uint8_t from=manager.headerFrom(); //Returns the FROM header of the last received message
+  uint8_t to=manager.headerTo(); //Returns the TO header of the last received message
+  uint8_t id=manager.headerId(); //Returns the ID of the last received message
+  uint8_t flags=manager.headerFlags(); //Returns the FLAGS of the last received message
+  int8_t rssi=rf95.lastRssi(); //Returns the POWER of the last received message
+  int count = 0; //Counts the number of packets received
   
+  //Receiving the message :
   bool res = manager.recvfromAck(buf, &len, &from);
+  //The function "recvfromAck" will listen for packets which are for it
+  //Here, it will listen for packets from CLIENT_1
+  //The fonction takes, in parameters, the buffer, the lenght of buffer, and the source
+  //Once executed, the function will return a boolean, 0 or 1
+  //These values will help us to see if the the packets are correctly received
   
   if (res == 0){
     
@@ -90,11 +105,8 @@ void loop(){
   else {
     Serial.println("Reception failed");
   }
-  Serial.println("Reboot ...");
+  //Putting Arduino to sleep for 1 sec (1000ms)
   delay(1000);
 }
 
-  
-  
-
-  
+//----------------------------------------------------------------------------//
